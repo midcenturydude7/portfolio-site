be1a2d3dcd8aead9d550fc402696a8f817d6c7c1
midCenturyDude7 <mgriffes@gmail.com>
2022-10-08 20:08:13 -0700

    Completed home page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function () {
+/* (function () {
   [...document.querySelectorAll(".control")].forEach(button => {
     button.addEventListener("click", function() {
       document.querySelector(".active-btn").classList.remove("active-btn");
@@ -10,4 +10,43 @@
   document.querySelector(".theme-btn").addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
   });
-})();
+})(); */
+
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controlls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
+
+function PageTransitions(){
+  for(let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener("click", function(){
+      let currentBtn = document.querySelectorAll(".active-btn");
+      currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+      this.className += " active-btn";
+    });
+  }
+
+  //Sections Active Class
+  for(let i = 0; i < allSections.length; i++){  
+    allSections[i].addEventListener("click", (e) => {
+      const id = e.target.dataset.id;
+      if(id){
+        //Remove selected from the other btns
+        sectBtns.forEach((btn) => {
+          btn.classList.remove("active");
+        });
+        e.target.classList.add("active");
+
+        // hide other sections
+        sections.forEach((section) => {
+          section.classList.remove("active");
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add("active");
+      }
+    });
+  }
+}
+
+PageTransitions();
